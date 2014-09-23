9d3251eb8ce218f2b382eb144ab1d90909f54b1e
Muhammad Iqbal Tawakal <tawakal@kth.se>
2014-09-23 16:37:08 +0200

    Fixed image index out of bound error

--- code/extract_feature_all_boxes.m
+++ code/extract_feature_all_boxes.m
@@ -8,12 +8,13 @@ function [boxes, features] = extract_feature_all_boxes(imgs,params)
         im = imread(imgs{i});
 
         disp(['selective search to image ', num2str(i)]);
+		tic
         boxes{i} = selective_search(im);
-
+		toc
         for j=1:length(boxes{i})
             box = boxes{i}(1, :);
             x = box(1); y = box(2); w = box(3); h = box(4);
-            patch = im(x:x+w, y:y+h);
+            patch = im(x:w, y:h);
             [rep, ~, ~] = caffe_features(patch, params);
             features{counter} = mean(rep, 2);
             counter = counter + 1;
