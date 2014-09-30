7c18331c6bc9769db721f1ed65044357fbbaad96
Muhammad Iqbal Tawakal <tawakal@kth.se>
2014-09-30 18:01:58 +0200

    test_selective_search produce result

--- code/test_selective_search.m
+++ code/test_selective_search.m
@@ -1,6 +1,6 @@
 setParams;
-
-%% The same function with train_selective_search but now the
+addpath('/home/iqbal/liblinear-1.93/matlab');
+% The same function with train_selective_search but now the
 % training is using data without region proposal (standard)
 
 % Load the extracted data
@@ -24,18 +24,26 @@ testF = sparse(testF);
 num_c = 12; % Number of different penalty parameter
 num_classes = 20; % Number of classes in VOC dataset
 
+c = 0.1;
+for i=1:num_c
+	Cs(i) = c;
+	c = c*2;
+end
+
 % It's prediction time
 for ci=1:num_c
 	for cli=1:num_classes
 
-		testL2 = zeros(length(testF));
-		[predicted_labels, accuracy, dec_value] = liblinear_predict(testL2, testF, models{cli, ci}, ['']);
+		testL2 = zeros(size(testF, 1), 1);
+		model = models{cli, ci};
+		[predicted_labels, accuracy, dec_value] = liblinear_predict(testL2, testF, model, ['']);
 
 		% Aggregate the result. There are two approaches, the maximum value and the average.
-		num_imgs = length(testL);
+		num_imgs = size(testL, 1);
+		%num_imgs = 10;
 		scores_sum = zeros(num_imgs, size(dec_value, 2));
 		scores_max = zeros(num_imgs, size(dec_value, 2));
-		boxes = cumsum(test_boxes);
+		boxes = cumsum(testBoxes);
 		for ii=1:num_imgs
 			if ii == 1
 				scores_sum(ii, :) = sum(dec_value(1:boxes(ii), :));
@@ -47,8 +55,8 @@ for ci=1:num_c
 		end
 
 		% Calculate precision, recall, and average precision
-		[rec_max{cli, ci}, prec_max{cli, ci}, aps_max(cli, ci)] = PR(scores_max, testL(:, cli));
-		[rec_sum{cli, ci}, prec_sum{cli, ci}, aps_sum(cli, ci)] = PR(scores_sum, testL(:, cli))
+		[rec_max{cli, ci}, prec_max{cli, ci}, aps_max(cli, ci)] = PR(scores_max, testL(:, cli), cli, ci);
+		[rec_sum{cli, ci}, prec_sum{cli, ci}, aps_sum(cli, ci)] = PR(scores_sum, testL(:, cli), cli, ci)
 	end
 end
 
