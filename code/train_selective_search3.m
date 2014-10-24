92d68b09fe86a8eec5dfcedfe27a9bfe6ab59dae
Muhammad Iqbal Tawakal <tawakal@kth.se>
2014-10-24 16:35:50 +0200

    Model can now be saved into file. Stupid mistake forgetting the quote

--- code/train_selective_search3.m
+++ code/train_selective_search3.m
@@ -1,12 +1,12 @@
 setParams;
 addpath('/home/iqbal/liblinear-1.93/matlab');
 
-for ii=1:20
+for ii=2:20
 	disp(['Now training class: ' num2str(ii)]);
 	[model, train_sums] = train_ss(ii);
 	for jj=1:size(model, 2)
 		models{ii, jj} = model{jj};
 	end
 
-	save('../models/caffe/VOC07-ss.mat', models, train_sums);
+	save('../models/caffe/VOC07-ss2.mat', 'models', 'train_sums');
 end
