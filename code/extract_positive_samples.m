796cd7ce03be28190b4c6cddbc87fae940e0355c
Muhammad Iqbal Tawakal <tawakal@kth.se>
2014-10-27 18:38:41 +0100

    Fixed bug after moving to production computer

--- code/extract_positive_samples.m
+++ code/extract_positive_samples.m
@@ -2,20 +2,23 @@
 
 setParams;
 addpath('/home/iqbal/caffe/matlab/caffe/');
+addpath('/home/iqbal/data/VOC07/VOCdevkit/');
+addpath('/home/iqbal/data/VOC07/VOCdevkit/VOCcode/');
 
 % Load all training images
-VOC07path = ['/home/iqbal/data/VOC07/'];
+VOC07path = '/home/iqbal/data/VOC07/';
 imdirpath = [VOC07path, 'VOCdevkit/VOC2007/JPEGImages/'];
-trainImgs = textread([VOC07path, 'VOCdevkit/VOC2007/ImageSets/Main/trainval.txt']);
+trainImgs = textread([VOC07path, 'VOCdevkit/VOC2007/ImageSets/Main/trainval.txt'], '%s');
 annopath = [VOC07path, 'VOCdevkit/VOC2007/Annotations/'];
 %num_train = length(trainImgs);
 num_train = 3;
 
 counter = zeros(20, 1);
-features = cell{20, 1};
+features = cell(20, 1);
 
 for ii=1:num_train
-	im = imread([imdirpath trainImgs{ii} '.jpg']);
+    impath = [imdirpath trainImgs{ii} '.jpg'];
+	im = imread(impath);
 	rec = PASreadrecord([annopath trainImgs{ii} '.xml']);
 
 	num_object = size(rec.objects, 2);
@@ -33,6 +36,8 @@ for ii=1:num_train
 
 		[rep, ~, ~] = caffe_features(patch, params);
 		counter(Index) = counter(Index)+1;
-		features{Index, counter(Index)};
+		features{Index, counter(Index)} = rep;
 	end
 end
+
+save('../data/caffe/positive_samples.mat', 'features');
