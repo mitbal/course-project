5ed1b43f95a00054694810dde64ba273f6ecb618
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-10-27 18:20:18 +0100

    Added script to extract positive samples given by the ground truth, bounding box annotation

---
+++ code/extract_positive_samples.m
@@ -0,0 +1,38 @@
+% Extract positive samples
+
+setParams;
+addpath('/home/iqbal/caffe/matlab/caffe/');
+
+% Load all training images
+VOC07path = ['/home/iqbal/data/VOC07/'];
+imdirpath = [VOC07path, 'VOCdevkit/VOC2007/JPEGImages/'];
+trainImgs = textread([VOC07path, 'VOCdevkit/VOC2007/ImageSets/Main/trainval.txt']);
+annopath = [VOC07path, 'VOCdevkit/VOC2007/Annotations/'];
+%num_train = length(trainImgs);
+num_train = 3;
+
+counter = zeros(20, 1);
+features = cell{20, 1};
+
+for ii=1:num_train
+	im = imread([imdirpath trainImgs{ii} '.jpg']);
+	rec = PASreadrecord([annopath trainImgs{ii} '.xml']);
+
+	num_object = size(rec.objects, 2);
+	for jj=1:num_object
+		label = rec.objects(jj).class;
+		IndexC = strfind(params.VOCclasses, label);
+		Index = find(not(cellfun('isempty', IndexC)));
+
+		x = rec.objects(jj).bndbox.xmin;
+		y = rec.objects(jj).bndbox.ymin;
+		w = rec.objects(jj).bndbox.xmax - x;
+		h = rec.objects(jj).bndbox.ymax - y;
+
+		patch = im(y:y+h, x:x+w);
+
+		[rep, ~, ~] = caffe_features(patch, params);
+		counter(Index) = counter(Index)+1;
+		features{Index, counter(Index)};
+	end
+end
