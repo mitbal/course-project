a11cba02d383dc092d10570e986796a2dc06a51f
Muhammad Iqbal Tawakal <tawakal@kth.se>
2014-11-02 12:42:02 +0100

    Fixed bug if the number of regions less than 4

--- code/extract_feature_all_boxes.m
+++ code/extract_feature_all_boxes.m
@@ -12,9 +12,9 @@ function [count_boxes, features] = extract_feature_all_boxes(imgs,params)
         disp(['Image: ', num2str(i)]);
 		tic
         boxes{i} = selective_search(im);
-		count_boxes(i) = length(boxes{i});
+		count_boxes(i) = size(boxes{i}, 1);
 		disp(['number of regions ', num2str(count_boxes(i))]);
-        for j=1:length(boxes{i})
+        for j=1:size(boxes{i}, 1)
             box = boxes{i}(j, :);
             y1 = box(1); x1 = box(2); y2 = box(3); x2 = box(4);
             patch = im(y1:y2, x1:x2, :);
